3f3e1f12f6aa1e02bd68cc32395209c888caae96
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-03-21 02:40:49 -0400

    Completed the challenge and add some test.

--- GCA_practice/BinaryPatternMatching.py
+++ GCA_practice/BinaryPatternMatching.py
@@ -80,21 +80,139 @@ Plan:
 def binaryPatternMatching(pattern, s):
 	vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 	counter = 0
+	bool_str = []
 
-	for i in range(len(s) - len(pattern)):
-		for j in range(len(pattern)):
-			window = s[i:i + j]
-			for pat in window:
-				if (s[i] in vowels) and (
-						pattern[j] == '0') or (
-						s[i] not in vowels) and (
-						pattern[j] != '0'):
-					if pattern[j:-1] in window:
-						counter += 1
+	for x in range(len(s)):
+		if s[x] in vowels:
+			bool_str.append(0)
+		else:
+			bool_str.append(1)
 
-	return counter // len(pattern)
+	print(bool_str)
+	print(f'Pattern: {pattern}')
+
+	for i in range(len(bool_str)):
+		pat_len = len(pattern)
+		temp_str = [str(x) for x in bool_str[i:i + pat_len]]
+		temp_str = ''.join(temp_str)
+		if temp_str == pattern:
+			counter += 1
+			print(f'Temp String {i}: {str(temp_str)}, Counter: {counter}')
+
+	return counter
+
+
+	#
+	# for i in range(len(s) - len(pattern)):
+	# 	for j in range(len(pattern)):
+	# 		window = s[i:i + j]
+	# 		for pat in window:
+	# 			if (s[i] in vowels) and (
+	# 					pattern[j] == '0') or (
+	# 					s[i] not in vowels) and (
+	# 					pattern[j] != '0'):
+	# 				if pattern[j:-1] in window:
+	# 					counter += 1
+	#
+	# return counter
+
+
+	# print(pat)
+	# print(let_str)
+	# s_dict = {k:v for k, v in enumerate(s)}
+	# count_dict = {}
+	# # print(s_dict)
+	#
+	# for i in range(len(s) - len(pattern)):
+	# 	for j in range(len(pattern)):
+	# 		if (pattern[j] == '0') and (s[i] in vowels) or (
+	# 				pattern[j] == '1') and (s[i] not in vowels):
+	# 			count_dict.update(i=True)
+	# 		else:
+	# 			count_dict[i] = 0
+	# 		print(count_dict)
+	#
+	# 		if count_dict.values() == 1:
+	# 			for x in range(len(pattern)):
+	# 				if len(count_dict.values()) == len(pattern):
+	# 					counter += 1
+	#
+	# return counter
+
+
+	# results = []
+	#
+	# for i in range(len(s) - len(pattern)):
+	# 	for j in range(len(pattern)):
+	# 		if (pattern[j] == '0' and s[i] in vowels) or (
+	# 				pattern[j] == '1' and s[i] not in vowels):
+	# 			results.append(True)
+	#
+	# 		else:
+	# 			results.append(False)
+	#
+	# print(results)
+
+
+
+#     begin_index = 0
+#     end_index = len(pattern)
+#     total = 0
+#     for x in range(len(num_string)):
+#         if pattern_list == num_string[begin_index:end_index]:
+#             total += 1
+#             begin_index += 1
+#             end_index += 1
+#         else:
+#             begin_index += 1
+#             end_index += 1
+#     return num_string
+
 
 
 if __name__ == '__main__':
-	print(binaryPatternMatching(pattern='010', s="amazing"))       # -> 2
-	print(binaryPatternMatching(pattern='100', s="codesignal"))    # -> 0
+
+	answers = {1: 2, 2: 0, 3: 2, 4: 2, 5: 2, 6: 2, 7: 0, 8: 4}
+
+	if binaryPatternMatching(pattern="010", s="amazing") == answers[1]:
+		print('Test #1 Passed!\n')
+
+	if binaryPatternMatching(pattern="100", s="codesignal") == answers[2]:
+		print('Test #2 Passed!\n')
+
+	if binaryPatternMatching(pattern="10", s="amazing") == answers[3]:
+		print('Test #3 Passed!\n')
+
+	if binaryPatternMatching(pattern="1010", s="codesignal") == answers[4]:
+		print('Test #4 Passed!\n')
+
+	if binaryPatternMatching(pattern="1101", s="cramming") == answers[5]:
+		print('Test #5 Passed!\n')
+
+	if binaryPatternMatching(pattern="0101", s="codesignal") == answers[6]:
+		print('Test #6 Passed!\n')
+
+	if binaryPatternMatching(pattern="11", s="amazin") == answers[7]:
+		print('Test #7 Passed!\n')
+
+	if binaryPatternMatching(pattern="01", s="codesignal") == answers[8]:
+		print('Test #8 Passed!')
+
+
+
+	# print(f'Test 1: {binaryPatternMatching(pattern="010", s="amazing")} --> '
+	#       f'2\n')  # -> 2
+	# print(f'Test 2: {binaryPatternMatching(pattern="100", s="codesignal")} --> '
+	#       f'0\n')  # -> 0
+	# print(f'Test 3: {binaryPatternMatching(pattern="10", s="amazing")} --> '
+	#       f'2\n')  # -> 2
+	# print(f'Test 4: {binaryPatternMatching(pattern="1010", s="codesignal")} '
+	#       f'--> 2\n')  # -> 2
+	# print(f'Test 5: {binaryPatternMatching(pattern="1101", s="cramming")} --> '
+	#       f'2\n')  # -> 2
+	# print(f'Test 6: {binaryPatternMatching(pattern="0101", s="codesignal")} '
+	#       f'--> 2\n')  # -> 2
+	# print(f'Test 7: {binaryPatternMatching(pattern="11", s="amazin")} --> '
+	#       f'0\n')  # -> 0
+	# print(f'Test 8: {binaryPatternMatching(pattern="01", s="codesignal")} '
+	#       f'--> 4')  # -> 4
