e6e12b833961b7f1c7e62730f1e156b89f4fe9a3
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-04-02 19:00:47 -0400

    Created file

---
+++ practice_exercises/BinaryStrSubstrRepresent1toN.py
@@ -0,0 +1,29 @@
+"""
+LeetCode: 1016: Binary String With Substrings Representing 1 To N
+(Medium difficulty)
+
+Given a binary string `S` (a string consisting only of '0' and '1's) and
+	a positive integer `N`, return `True` if and only if, for every integer `X`
+	from `1` to `N`, the binary representation of `X` is a substring of `S`.
+
+Example 1:
+
+	Input: `S = "0110", N = 3`
+	Output: `True`
+
+Example 2:
+
+	Input: `S = "0110", N = 4`
+	Output: `False`
+
+Note:
+
+	`1 <= S.length <= 1000`
+	`1 <= N <= 10^9`
+"""
+"""
+
+"""
+
+
+def query_string(S, N):
