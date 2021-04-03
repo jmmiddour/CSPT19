c5d7f1916bc91d7dc12f519c414048ac9f39e442
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-04-02 23:00:31 -0400

    Solved the problem

--- practice_exercises/BinaryStrSubstrRepresent1toN.py
+++ practice_exercises/BinaryStrSubstrRepresent1toN.py
@@ -22,8 +22,65 @@ Note:
 	`1 <= N <= 10^9`
 """
 """
+Test Cases:
+S = '0110'
+N = 3
+output = True
+explanation --> 3 = 11 in binary --> 11 is a substring of S
+                2 = 10 in binary --> 10 is a substring of S
+                1 = 1 in binary --> 1 is a substring of S
 
+S = '0110'
+N = 4
+Output = False
+explanation --> 4 = 100 in binary --> not a substring of S (can return False at this step)
+                3 = 11 in binary --> 11 is a substring of S
+                2 = 10 in binary --> 10 is a substring of S
+                1 = 1 in binary --> 1 is a substring of S
+
+S = '0110111001'
+N = 13
+Output = False
+explanation --> 13 = 1101 in binary --> 1101 is a substrong of S
+                12 = 1100 in binary --> 1100 is a substring of S
+                11 = 1011 in binary --> 1011 is a substring of S
+                10 = 1010 in binary --> 1010 is not a substring of S (return False here)
+
+Plan:
+1. Create a list of binary values to represent 1 - N
+2. Iterate through the string S to check that all binary values are in the string.
+3. Return True if all binary values are in S, False if not all match.    
 """
 
 
 def query_string(S, N):
+	# ### This code works but exceeded the time limit ### #
+	# ### NEED TO REFACTOR FOR FASTER TIME ### #
+	# bin_vals = [format(int(x), 'b') for x in range(1, N + 1)]
+	# # print(bin_vals)
+	# results = 0
+	#
+	# for i in range(len(bin_vals)):
+	# 	if bin_vals[i] in S:
+	# 		results += 1
+	#
+	# return results == len(bin_vals)
+
+	# ### Code found at https://leetcode.com/problems/binary-string-with
+# -substrings-representing-1-to-n/discuss/260847/JavaC%2B%2BPython-O(S) ### #
+	if int(S) == 1:
+		return True
+
+	return all(bin(i)[2:] in S for i in range(N, N // 2 - 1, -1))
+
+
+# Testing:
+if __name__ == '__main__':
+	S = "011010101010111101010101011111111111111111111111111111111110000000000000011111101010101001010101010101010101010101111010101010111111111111111111111111111111111100000000000000111111010101010010101010101010101010100"
+	N = 1000000000
+
+	if query_string(S, N) == False:
+		print('Correct!')
+
+	else:
+		print('Wrong! Expected: False')
