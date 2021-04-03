fc7ffda32b221cfa49301823d5aa2c1cdb16869a
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-04-03 18:44:19 -0400

    Solved the problem and added test cases

--- practice_exercises/MatrixElementsSum.py
+++ practice_exercises/MatrixElementsSum.py
@@ -22,7 +22,8 @@ For matrix = [[0, 1, 1, 2],
 	the output should be:
 		matrixElementsSum(matrix) = 9.
 
-https://codesignal.s3.amazonaws.com/tasks/matrixElementsSum/img/example1.png?_tm=1582038746746
+https://codesignal.s3.amazonaws.com/tasks/matrixElementsSum/img/example1.png
+?_tm=1582038746746
 
 	There are several haunted rooms, so we'll disregard them as well as any
 		rooms beneath them. Thus, the answer is 1 + 5 + 1 + 2 = 9.
@@ -34,7 +35,8 @@ For matrix = [[1, 1, 1, 0],
 	the output should be:
 		matrixElementsSum(matrix) = 9.
 
-https://codesignal.s3.amazonaws.com/tasks/matrixElementsSum/img/example2.png?_tm=1582038747009
+https://codesignal.s3.amazonaws.com/tasks/matrixElementsSum/img/example2.png
+?_tm=1582038747009
 
 	Note that the free room in the final column makes the full column
 		unsuitable for bots (not just the room directly beneath it). Thus,
@@ -59,7 +61,133 @@ Input/Output:
 """
 
 """
+Test Cases:
+matrix = [[0, 1, 1, 2],
+          [0, 5, 0, 0],
+          [2, 0, 3, 3]]
+OUTPUT --> 9
+Expanation --> 1 + 1 + 2 + 5 = 9 (these are the only values not below a 0 in 
+the matrix above)
 
+matrix = [[0, 8, 3, 9],
+          [7, 0, 6, 5],
+          [1, 1, 0, 9]]
+OUTPUT --> 40
+Expanation --> 8 + 3 + 9 + 6 + 5 + 9 = 40 (these are the only values not 
+below a 0 in the matrix above)
+
+Plan:
+1. Create a list to hold the sum.
+2. Iterate through the rows and columns of the matrix.
+3. If the value is not 0 or positioned below a 0 in the matrix, increament 
+the sum by the value at that position.
+4. Return the final sum of the valid rooms.
 """
 
+
 def matrixElementsSum(matrix):
+	# Create a variable to hold the current sum
+	result = 0
+	# Create a list to hold the columns that already saw a 0
+	bad_col = []
+
+	# Iterate through the matrix rows
+	for row in range(len(matrix)):
+		# Iterate through the matrix columns
+		for col in range(len(matrix[row])):
+			# If the current value is 0
+			if matrix[row][col] == 0:
+				# Add it to the bad column list
+				bad_col.append(col)
+
+			# If the current value is not 0 and
+			#   the column is not in the bad column list
+			if matrix[row][col] != 0 and col not in bad_col:
+				# Increase the total sum by the current value
+				result += matrix[row][col]
+
+	# Return the total sum of valid rooms
+	return result
+
+
+# Testing:
+if __name__ == '__main__':
+	m = [[0, 1, 1, 2], [0, 5, 0, 0], [2, 0, 3, 3]]
+	ans = 9
+	if matrixElementsSum(m) == ans:
+		print(f'Test 1 PASSED! Answer is: {ans}')
+	else:
+		print(f'Test 1 FAILED!\nYour Output: {matrixElementsSum(m)}\n '
+		      f'Correct: {ans}')
+
+	m = [[1,1,1,0], [0,5,0,1], [2,1,3,10]]
+	ans = 9
+	if matrixElementsSum(m) == ans:
+		print(f'Test 2 PASSED! Answer is: {ans}')
+	else:
+		print(f'Test 2 FAILED!\nYour Output: {matrixElementsSum(m)}\n '
+		      f'Correct: {ans}')
+
+	m = [[1,1,1], [2,2,2], [3,3,3]]
+	ans = 18
+	if matrixElementsSum(m) == ans:
+		print(f'Test 3 PASSED! Answer is: {ans}')
+	else:
+		print(f'Test 3 FAILED!\nYour Output: {matrixElementsSum(m)}\n '
+		      f'Correct: {ans}')
+
+	m = [[0]]
+	ans = 0
+	if matrixElementsSum(m) == ans:
+		print(f'Test 4 PASSED! Answer is: {ans}')
+	else:
+		print(f'Test 4 FAILED!\nYour Output: {matrixElementsSum(m)}\n '
+		      f'Correct: {ans}')
+
+	m = [[1, 0, 3], [0, 2, 1], [1, 2, 0]]
+	ans = 5
+	if matrixElementsSum(m) == ans:
+		print(f'Test 5 PASSED! Answer is: {ans}')
+	else:
+		print(f'Test 5 FAILED!\nYour Output: {matrixElementsSum(m)}\n '
+		      f'Correct: {ans}')
+
+	m = [[1], [5], [0], [2]]
+	ans = 6
+	if matrixElementsSum(m) == ans:
+		print(f'Test 6 PASSED! Answer is: {ans}')
+	else:
+		print(f'Test 6 FAILED!\nYour Output: {matrixElementsSum(m)}\n '
+		      f'Correct: {ans}')
+
+	m = [[1, 2, 3, 4, 5]]
+	ans = 15
+	if matrixElementsSum(m) == ans:
+		print(f'Test 7 PASSED! Answer is: {ans}')
+	else:
+		print(f'Test 7 FAILED!\nYour Output: {matrixElementsSum(m)}\n '
+		      f'Correct: {ans}')
+
+	m = [[2], [5], [10]]
+	ans = 17
+	if matrixElementsSum(m) == ans:
+		print(f'Test 8 PASSED! Answer is: {ans}')
+	else:
+		print(f'Test 8 FAILED!\nYour Output: {matrixElementsSum(m)}\n '
+		      f'Correct: {ans}')
+
+	m = [[4, 0, 1], [10, 7, 0], [0, 0, 0], [9, 1, 2]]
+	ans = 15
+	if matrixElementsSum(m) == ans:
+		print(f'Test 9 PASSED! Answer is: {ans}')
+	else:
+		print(f'Test 9 FAILED!\nYour Output: {matrixElementsSum(m)}\n '
+		      f'Correct: {ans}')
+
+	m = [[1]]
+	ans = 1
+	if matrixElementsSum(m) == ans:
+		print(f'Test 10 PASSED! Answer is: {ans}')
+	else:
+		print(f'Test 10 FAILED!\nYour Output: {matrixElementsSum(m)}\n '
+		      f'Correct: {ans}')
