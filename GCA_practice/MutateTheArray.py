9ae29fbdfe9e0dcc00dda4248e35d666933f3752
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-04-05 17:04:00 -0400

    Added another solution I did today and some more test cases.

--- GCA_practice/MutateTheArray.py
+++ GCA_practice/MutateTheArray.py
@@ -71,7 +71,122 @@ def mutateTheArray(n, a):
 	return b  # Returns the mutated array `b`
 
 
+# ### NEW SOLUTION 04-05-2021 ### #
+"""
+- Create a new list `b` that is empty
+- Check for edge case of only one value in `a`
+- If `a[i]` at index 0, then `b[i] = a[i] + a[i + 1]`, append the sum to `b`
+- If `a[i]` at the last index, then `b[i] = a[i - 1] + a[i]`, append the sum 
+to `b`
+- Else, `b[i] = a[i - 1] + a[i] + a[i + 1]`, append the sum to `b`
+- Return the mutated array `b`
+"""
+
+
+def mutateTheArray(n, a):
+	# Create an empty list to store the resulting sums
+	b = []
+
+	# If there is only one element in `a`
+	if n < 2:
+		return a
+
+	# If at the first element in the list
+	if a[0]:
+		# Append the sum of the first 2 elements in the list
+		b.append(a[0] + a[1])
+
+	# Iterate through the list from the second element to the next to last
+	# element
+	for i in range(1, len(a) - 1):
+		# Append the sum to the list fro results
+		b.append(a[i - 1] + a[i] + a[i + 1])
+
+	# If at the last element in the list
+	if a[-1]:
+		# Append the sum of the last 2 elements
+		b.append(a[-2] + a[-1])
+
+	# Return the results list
+	return b
+
+
 # Test Cases:
 if __name__ == '__main__':
 	print(mutateTheArray(5, [4, 0, 1, -2, 3]))      # -> [4, 5, -1, 2, 1]
 	print(mutateTheArray(6, [3, 5, 8, 3, 9, 6]))    # -> [8, 16, 16, 20, 18, 15]
+
+
+"""
+Test from CodeSignal:
+
+Test 1
+Input:
+n: 5
+a: [4, 0, 1, -2, 3]
+Expected Output:
+[4, 5, -1, 2, 1]
+
+Test 2
+Input:
+n: 1
+a: [9]
+Expected Output:
+[9]
+
+Test 3
+Input:
+n: 4
+a: [1, 2, 3, 4]
+Expected Output:
+[3, 6, 9, 7]
+
+Test 4
+Input:
+n: 9
+a: [-9, -8, 7, 7, 7, 6, -6, 4, 6]
+Expected Output:
+[-17, -10, 6, 21, 20, 7, 4, 4, 10]
+
+Test 5
+Input:
+n: 10
+a: [6, -5, -5, 5, 10, 5, 1, 8, 6, -2]
+Expected Output:
+[1, -4, -5, 10, 20, 16, 14, 15, 12, 4]
+
+Test 6
+Input:
+n: 10
+a: [1, 10, 10, -6, 5, -3, -7, 10, 9, 10]
+Expected Output:
+[11, 21, 14, 9, -4, -5, 0, 12, 29, 19]
+
+Test 7
+Input:
+n: 4
+a: [-6, -5, -7, -1]
+Expected Output:
+[-11, -18, -13, -8]
+
+Test 8
+Input:
+n: 8
+a: [-4, -5, 0, -6, -5, -4, -7, 9]
+Expected Output:
+[-9, -9, -11, -11, -15, -16, -2, 2]
+
+Test 9
+Input:
+n: 1
+a: [-5]
+Expected Output:
+[-5]
+
+Test 10
+Input:
+n: 5
+a: [-9, -6, 10, -6, -10]
+Expected Output:
+[-15, -5, -2, -6, -16]
+"""
