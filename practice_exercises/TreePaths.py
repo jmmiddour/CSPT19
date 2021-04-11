e0d639744d6f7f3d46331aa3da17228bd56effd7
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-04-11 17:15:10 -0400

    Solved the problem, added test cases.

--- practice_exercises/TreePaths.py
+++ practice_exercises/TreePaths.py
@@ -59,3 +59,87 @@ Input/Output:
 		appear in the pre-order traversal (i.e. from the leftmost leaf
 		to the rightmost).
 """
+
+
+#
+# Binary trees are already defined with this interface:
+class Tree(object):
+	def __init__(self, x):
+		self.value = x
+		self.left = None
+		self.right = None
+
+
+def treePaths(t):
+	"""
+	Understanding:
+	- Traverse the tree in a pre-order depth-first manner.
+	- Need to return the values in a list of strings
+	- Strings need to be in the format:
+		"root->child1->child2->...->leaf"
+	"""
+	# Check for edge case of no tree node
+	if t is None:
+		# Return an empty list
+		return []
+
+	# If there is no children, it is a leaf
+	if t.left == None and t.right == None:
+		# and need to return just the leaf node
+		return [str(t.value)]
+
+	# Using recursion, get root node value, root's left, then right and add it
+	# all as one string per path.
+	return [str(t.value) + '->' + x for x in
+	        treePaths(t.left) + treePaths(t.right)]
+
+# Testing
+if __name__ == '__main__':
+	# Test 1
+	t = Tree(5)
+	t.left = Tree(2)
+	t.left.left = Tree(10)
+	t.left.right = Tree(4)
+	t.right = Tree(-3)
+	ans = ["5->2->10", "5->2->4", "5->-3"]
+	if ans == treePaths(t):
+		print(f'PASSED!\n')
+	else:
+		print(f'Failed:\nYour Output --> {treePaths(t)}\nCorrect Output -->'
+		      f' {ans}\n')
+
+	# Test 2
+	t = None
+	ans = []
+	if ans == treePaths(t):
+		print(f'PASSED!\n')
+	else:
+		print(f'Failed:\nYour Output --> {treePaths(t)}\nCorrect Output -->'
+		      f' {ans}\n')
+
+	# Test 3
+	t = Tree(42)
+	ans = ['42']
+	if ans == treePaths(t):
+		print(f'PASSED!\n')
+	else:
+		print(f'Failed:\nYour Output --> {treePaths(t)}\nCorrect Output -->'
+		      f' {ans}\n')
+
+	# Test 4
+	t = Tree(1000)
+	t.left = Tree(2)
+	t.left.left = Tree(10)
+	t.left.left.right = Tree(-1000)
+	t.left.left.right.right = Tree(0)
+	t.left.right = Tree(4)
+	t.right = Tree(3)
+	t.right.right = Tree(99)
+	t.right.right.left = Tree(6)
+	t.right.right.left.left = Tree(1)
+	ans = ["1000->2->10->-1000->0", "1000->2->4", "1000->3->99->6->1"]
+	if ans == treePaths(t):
+		print(f'PASSED!\n')
+	else:
+		print(f'Failed:\nYour Output --> {treePaths(t)}\nCorrect Output -->'
+		      f' {ans}\n')
