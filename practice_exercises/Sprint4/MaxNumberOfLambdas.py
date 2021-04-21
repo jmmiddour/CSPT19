003740f83b6f15304e7efd2763e1565b2e18b81d
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-04-21 15:43:47 -0400

    added solutions

--- practice_exercises/Sprint4/MaxNumberOfLambdas.py
+++ practice_exercises/Sprint4/MaxNumberOfLambdas.py
@@ -32,8 +32,74 @@ Notes:
 [output] integer
 """
 
-def csMaxNumberOfLambdas(text):
 
+# def csMaxNumberOfLambdas(text):
+# 	# Create a dictionary that will have the letters of "lambda" as the keys
+# 	letters = {'l': 0, 'a': 0, 'm': 0, 'b': 0, 'd': 0}
+# 	# Create a counter to keep track of how many "Lambda"'s I can get
+# 	counter = 0
+#
+# 	# Iterate through the string of text
+# 	for i in range(len(text)):
+# 		# If the current character is l, a, m, b, or d...
+# 		if text[i] == 'l' or text[i] == 'a' or text[i] == 'm' or text[
+# 			i] == 'b' or text[i] == 'd':
+# 			# Increase the value for that key in the dictionary by 1
+# 			letters[text[i]] += 1
+#
+# 	# While key values are > 0
+# 	while all(letters.values()) > 0:
+# 		# If the value at key (l, m, b, d) >= 1 and value at key(a) >=2...
+# 		if letters['l'] >= 1 and letters['m'] >= 1 and letters['b'] >= 1 and \
+# 				letters['d'] >= 1 and letters['a'] >= 2:
+# 			# Increment the counter by 1
+# 			counter += 1
+# 			# Decrement the value at key (l, m, b, d) by 1
+# 			letters['l'] -= 1
+# 			letters['m'] -= 1
+# 			letters['b'] -= 1
+# 			letters['d'] -= 1
+# 			# Decrement the value at key (a) by 2
+# 			letters['a'] -= 2
+#
+# 	# Return the counter
+# 	return counter
+
+
+###########################################################################
+# ################# Another Little Cleaner Solution ##################### #
+###########################################################################
+
+def csMaxNumberOfLambdas(text):
+	# Create a dictionary that will have the letters of "lambda" as the keys
+	letters = {'l': 0, 'a': 0, 'm': 0, 'b': 0, 'd': 0}
+	# Create a counter to keep track of how many "Lambda"'s I can get
+	counter = 0
+
+	# Iterate through the string of text
+	for i in range(len(text)):
+		# If the current character is l, a, m, b, or d...
+		if text[i] in letters.keys():
+			# Increase the value for that key in the dictionary by 1
+			letters[text[i]] += 1
+
+	# While key values are > 0
+	while all(letters.values()) > 0:
+		# If the value at key (l, m, b, d) >= 1 and value at key(a) >=2...
+		if (letters['l'] and letters['m'] and letters['b'] and letters[
+			'd']) >= 1 and letters['a'] >= 2:
+			# Increment the counter by 1
+			counter += 1
+			# Decrement the value at key (l, m, b, d) by 1
+			letters['l'] -= 1
+			letters['m'] -= 1
+			letters['b'] -= 1
+			letters['d'] -= 1
+			# Decrement the value at key (a) by 2
+			letters['a'] -= 2
+
+	# Return the counter
+	return counter
 
 
 # Testing:
@@ -60,7 +126,7 @@ if __name__ == '__main__':
 	text = "sctlamb"
 	ans = 0
 	if csMaxNumberOfLambdas(text) == ans:
-		print('Test 3 PASSED!!!\n')
+		print('Test 3 PASSED!!!')
 	else:
 		print(f'Test 3 Failed!\n  Your Output: {csMaxNumberOfLambdas(text)}\n'
 		      f'  Correct Output: {ans}')
